2c84d668744ec3931d27041a56f45f42b0a46f56
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2024-03-14 08:45:40 -0500

    Solucionado undefined de saludables en menuCuc

--- src/sedeCuc/components/MenuSaludable.jsx
+++ src/sedeCuc/components/MenuSaludable.jsx
@@ -81,53 +81,59 @@ export const MenuSaludable = ({ title, items = [], batidos, parfaits }) => {
           );
         })}
       </div>
-      <div className="text-start mt-5 mb-3 bg-[#8B9F71] w-auto py-1 px-2 rounded-lg">
-        <Typography variant="h6" fontFamily={"'Poppins', sans-serif"} color={"black"} fontWeight={900}>
-          {parfaits.description}:
-        </Typography>
-      </div>
-      <div>
-        <div className=" sm:flex  justify-start text-start">
-          {parfaits.items?.map((value, i) => {
-            return (
-              <div key={i} className="my-1 pr-3">
-                <Card sx={{ display: "flex", backgroundColor: "#8B9F71" }}>
-                  <div className="flex flex-col w-full">
-                    <CardContent sx={{ flex: "1 0 auto" }}>
-                      <Typography component="div" variant="h5" fontFamily={"'Poppins', sans-serif"} fontWeight={"bold"}>
-                        {value.name}
-                      </Typography>
-                      <Typography variant="subtitle1" color="whitesmoke" component="div" fontFamily={"'Poppins', sans-serif"}>
-                        {value.description}
-                      </Typography>
-                    </CardContent>
+      {parfaits?.description != undefined && parfaits != undefined ? (
+        <>
+          <div className="text-start mt-5 mb-3 bg-[#8B9F71] w-auto py-1 px-2 rounded-lg">
+            <Typography variant="h6" fontFamily={"'Poppins', sans-serif"} color={"black"} fontWeight={900}>
+              {parfaits.description}:
+            </Typography>
+          </div>
+          <div>
+            <div className=" sm:flex  justify-start text-start">
+              {parfaits.items?.map((value, i) => {
+                return (
+                  <div key={i} className="my-1 pr-3">
+                    <Card sx={{ display: "flex", backgroundColor: "#8B9F71" }}>
+                      <div className="flex flex-col w-full">
+                        <CardContent sx={{ flex: "1 0 auto" }}>
+                          <Typography component="div" variant="h5" fontFamily={"'Poppins', sans-serif"} fontWeight={"bold"}>
+                            {value.name}
+                          </Typography>
+                          <Typography variant="subtitle1" color="whitesmoke" component="div" fontFamily={"'Poppins', sans-serif"}>
+                            {value.description}
+                          </Typography>
+                        </CardContent>
+                      </div>
+                    </Card>
                   </div>
-                </Card>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className=" sm:flex  justify-start text-start">
-        {parfaits.size?.map((value, i) => {
-          return (
-            <div key={i} className="my-1 pr-3">
-              <Card sx={{ display: "flex", backgroundColor: "#8B9F71" }}>
-                <div className="flex flex-col w-full">
-                  <CardContent sx={{ flex: "1 0 auto", display: "flex", justifyContent: "space-between" }}>
-                    <Typography component="div" variant="h6" fontFamily={"'Poppins', sans-serif"}>
-                      {value.name}
-                    </Typography>
-                    <Typography variant="h6" component="div" fontFamily={"'Poppins', sans-serif"} alignSelf={"end"} fontWeight={"bold"}>
-                      ${value.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
-                    </Typography>
-                  </CardContent>
-                </div>
-              </Card>
+                );
+              })}
             </div>
-          );
-        })}
-      </div>
+          </div>
+          <div className=" sm:flex  justify-start text-start">
+            {parfaits.size?.map((value, i) => {
+              return (
+                <div key={i} className="my-1 pr-3">
+                  <Card sx={{ display: "flex", backgroundColor: "#8B9F71" }}>
+                    <div className="flex flex-col w-full">
+                      <CardContent sx={{ flex: "1 0 auto", display: "flex", justifyContent: "space-between" }}>
+                        <Typography component="div" variant="h6" fontFamily={"'Poppins', sans-serif"}>
+                          {value.name}
+                        </Typography>
+                        <Typography variant="h6" component="div" fontFamily={"'Poppins', sans-serif"} alignSelf={"end"} fontWeight={"bold"}>
+                          ${value.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                        </Typography>
+                      </CardContent>
+                    </div>
+                  </Card>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
