19eb54f99c1f43deffee1ea6495429ab1ea541d3
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2024-03-17 22:51:27 -0500

    Solucionadoi problema del footer, cambiado a relative nuevamente

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const MainPage = () => {
   return (
-    <div className="mt-14  mx-auto  w-full h-auto">
+    <div className="mt-14  mx-auto  w-full h-full">
       <div className="bg-[#FECE00] hover:bg-yellow-400 w-3/6 mx-auto rounded-full mb-5 border-yellow-500 border-r-[1px] border-t-[1px] border-l-[1px]  border-b-[6px]">
         <NavLink to="/villavicencio">
           <Button fullWidth sx={{ color: "black", fontWeight: "bold" }}>
